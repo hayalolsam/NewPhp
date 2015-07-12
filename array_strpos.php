781eae9e5baa8c6f2404cab7cf44a9cc1e30a59f
hayalolsam <hayalolsam.org@hotmail.com>
2015-07-12 09:26:07 +0300

    Update array_strpos.php

--- array_strpos.php
+++ array_strpos.php
@@ -1,5 +1,6 @@
 <?php
-function array_strpos($haystack, $needle, $offset=0) {
+function array_strpos($haystack, $needle, $offset=0)
+{
     if(!is_array($needle)) $needle = array($needle);
     foreach($needle as $query) {
         if(strpos($haystack, $query, $offset) !== false) return true;
