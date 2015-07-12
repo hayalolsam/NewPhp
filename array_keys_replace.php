e50972e22afd4fc8ff4dc27570208930fa77b99a
hayalolsam <hayalolsam.org@hotmail.com>
2015-07-12 09:24:15 +0300

    Update array_keys_replace.php

--- array_keys_replace.php
+++ array_keys_replace.php
@@ -1,10 +1,10 @@
 <?php 
 function array_keys_replace($old,$new,$array)
 {
-    $keys = array_keys($array);
-		$values = array_values($array);
-		$new_keys = str_replace($old, $new, $keys);
-		return array_combine($new_keys, $values);
+	$keys = array_keys($array);
+	$values = array_values($array);
+	$new_keys = str_replace($old, $new, $keys);
+	return array_combine($new_keys, $values);
 }
 
 /*
