f4e2c80f62735ca5ef1724b6edc24d54fb036dab
hayalolsam <hayalolsam.org@hotmail.com>
2015-07-12 09:24:31 +0300

    Update array_values_replace.php

--- array_values_replace.php
+++ array_values_replace.php
@@ -1,10 +1,10 @@
 <?php 
 function array_values_replace($old,$new,$array)
 {
-    $keys = array_keys($array);
-		$values = array_values($array);
-		$new_values = str_replace($old, $new, $values);
-		return array_combine($keys, $new_values);
+	$keys = array_keys($array);
+	$values = array_values($array);
+	$new_values = str_replace($old, $new, $values);
+	return array_combine($keys, $new_values);
 }
 
 /*
