dc00314f468fd27b1053d5a0a41350c2347a8eec
hayalolsam <hayalolsam.org@hotmail.com>
2015-07-12 09:23:29 +0300

    Update array_values_replace.php

--- array_values_replace.php
+++ array_values_replace.php
@@ -1,7 +1,14 @@
-function array_values_replace($old,$new,$array)
+<?php function array_values_replace($old,$new,$array)
 {
     $keys = array_keys($array);
 		$values = array_values($array);
 		$new_values = str_replace($old, $new, $values);
 		return array_combine($keys, $new_values);
 }
+
+/*
+$array=array("world"=>"ok");
+$array=array_values_replace("ok","no",$array);
+var_dump($array); // return array(1) { ["world"]=> string(2) "no" }
+*/
+?>
